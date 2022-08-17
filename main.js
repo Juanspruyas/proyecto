3869e6f0d135f5d66cd5477a735b8ae1aee1f6a6
Juan Segundo Pruyas <juanspruyas@gmail.com>
2022-08-17 19:12:34 -0300

    primera entrega proyecto

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let diaDescuento = prompt("Ingrese el dia de la semana que se encuentra para ver en que productos TIENE UN 20% DE DESCUENTO");
+/*let diaDescuento = prompt("Ingrese el dia de la semana que se encuentra para ver en que productos TIENE UN 20% DE DESCUENTO");
 while(diaDescuento != "terminado"){
 switch (diaDescuento) {
     case "lunes":
@@ -35,10 +35,56 @@ switch (diaDescuento) {
         diaDescuento =  prompt("Al culminar ingrese la palabra \"terminado\" para continuar su busqueda"); }
 
 
-let precio = parseFloat(prompt("Ingrese precio del producto seleccionado para ver el precio con descuento "));
-let descuento20 = precio - (precio * 0.2);
+        function calcularDescuento (){
+            return precio - precio * 0.2
+        }
+
+
+        let precio = parseFloat(
+            prompt("Ingrese precio del producto seleccionado para ver el precio con descuento "));
+let descuento20 = calcularDescuento(precio);
 let precioFinal = 
     alert ("El valor del producto seria $" + descuento20 + " con el descuento incluido");
 
-console.log(descuento20);
+console.log(descuento20);*/
+
+
+class producto{
+    constructor(nombre, precio, cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+
+    }
+}
+
+
+var arrayProductos = [];
+do{
+    var comprobacion = prompt("ingrese el producto o terminado para finalizar");
+    if (comprobacion === "terminado" ||comprobacion === "Terminado"||comprobacion === "TERMINADO"){
+        break;
+    }else{
+        nombreP = comprobacion;
+        var precioP = prompt("ingrese el precio del producto");
+        var cantidadP = prompt ("ingrese la cantidad de productos");
+        arrayProductos.push (new producto(nombreP, precioP, cantidadP));
+    }
+}
+while (comprobacion != "terminado" ||comprobacion != "Terminado"||comprobacion != "TERMINADO")
+
+console.log(arrayProductos);
+
+for(var producto of arrayProductos){
+    document.write("<h3> El producto ingresado es: " + producto.nombre + "</h3>");
+    document.write("<h3> La cantidad del producto ingresado es: " + producto.cantidad + "</h3>");
+    document.write("<h3> el precio del producto ingresado es " + (precioP * cantidadP)+"</h3>" );
+
+}
+
+console.log(producto.nombre);
+console.log(producto.cantidad);
+console.log(precioP * cantidadP);
+
+
 
